4243b744e07daa8d9ae0612ca354f43c29361584
Jhon Carlos Acevedo Mendoza <jhonacevedomendoza@gmail.com>
2022-11-02 11:10:34 -0500

    styles

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 const URL = "https://animechan.vercel.app/api/random";
+const URL2 = "https://api.waifu.pics/sfw/smile"; //agregar peticion a character
+// https://waifu.pics/docs usar API-REST info en el link
 
 fetch(URL)
   .then((response) => response.json())
   // .then((quote) => console.log(quote))
-  .then(data => {
-  document.getElementById("txt").innerHTML = data.anime;
-  document.getElementById("txt2").innerHTML = data.character;
-  document.getElementById("txt3").innerHTML = data.quote;
-  })
+  .then((data) => {
+    document.getElementById("txt").innerHTML = data.anime;
+    document.getElementById("txt2").innerHTML = data.character;
+    document.getElementById("txt3").innerHTML = data.quote;
+  });
